afdc17c1dbf216311f001d6ca923fba6563200f1
takabow0705 <hrgc.takahiro@outlook.jp>
2023-11-12 23:18:42 +0900

    add tokentype

--- jLox/src/main/java/com/github/takabow0705/craftinginterpreters/lox/TokenType.java
+++ jLox/src/main/java/com/github/takabow0705/craftinginterpreters/lox/TokenType.java
@@ -1,4 +1,20 @@
 package com.github.takabow0705.craftinginterpreters.lox;
 
 public enum TokenType {
+    //記号一個のトークン
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    //記号一個または二個によるトークン
+    BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+
+    // リテラル
+    IDENTIFIER, STRING, NUMBER,
+
+    // キーワード
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
 }
