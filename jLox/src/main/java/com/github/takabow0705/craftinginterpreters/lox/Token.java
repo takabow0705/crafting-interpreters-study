b1b3da92ea252b2a7d96bcc833a06d3067079eda
takabow0705 <hrgc.takahiro@outlook.jp>
2023-11-12 23:21:31 +0900

    add token object

--- jLox/src/main/java/com/github/takabow0705/craftinginterpreters/lox/Token.java
+++ jLox/src/main/java/com/github/takabow0705/craftinginterpreters/lox/Token.java
@@ -1,4 +1,20 @@
 package com.github.takabow0705.craftinginterpreters.lox;
 
 public class Token {
+    final TokenType tokenType;
+    final String lexme;
+    final Object literal;
+    final int line;
+
+    public Token(TokenType tokenType, String lexme, Object literal, int line) {
+        this.tokenType = tokenType;
+        this.lexme = lexme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    @Override
+    public String toString() {
+        return tokenType + " " + lexme + " " + literal;
+    }
 }
