fc8ea6e2c7df911a501a49bf9325b5adcfc1251d
takabow0705 <hrgc.takahiro@outlook.jp>
2023-11-19 22:08:13 +0900

    commit gerated AST class

--- jLox/src/main/java/com/github/takabow0705/craftinginterpreters/lox/Expr.java
+++ jLox/src/main/java/com/github/takabow0705/craftinginterpreters/lox/Expr.java
@@ -1,15 +1,43 @@
 package com.github.takabow0705.craftinginterpreters.lox;
 
+import java.util.List;
+
 abstract class Expr {
     static class Binary extends Expr {
-        Binary(Expr left, Token token, Expr right) {
+        Binary(Expr left, Token operator, Expr right) {
             this.left = left;
-            this.token = token;
+            this.operator = operator;
             this.right = right;
         }
 
         final Expr left;
-        final Token token;
+        final Token operator;
+        final Expr right;
+    }
+
+    static class Grouping extends Expr {
+        Grouping(Expr expression) {
+            this.expression = expression;
+        }
+
+        final Expr expression;
+    }
+
+    static class Literal extends Expr {
+        Literal(Object object) {
+            this.object = object;
+        }
+
+        final Object object;
+    }
+
+    static class Unary extends Expr {
+        Unary(Token operator, Expr right) {
+            this.operator = operator;
+            this.right = right;
+        }
+
+        final Token operator;
         final Expr right;
     }
 
