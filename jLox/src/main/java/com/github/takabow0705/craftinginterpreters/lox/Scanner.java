2c4901edffe0758ec0fdcfc8bc4abae36c37f9de
takabow0705 <hrgc.takahiro@outlook.jp>
2023-11-19 17:25:33 +0900

    finish string literal parsing function(p60)

--- jLox/src/main/java/com/github/takabow0705/craftinginterpreters/lox/Scanner.java
+++ jLox/src/main/java/com/github/takabow0705/craftinginterpreters/lox/Scanner.java
@@ -1,15 +1,149 @@
 package com.github.takabow0705.craftinginterpreters.lox;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
+
+import static com.github.takabow0705.craftinginterpreters.lox.TokenType.*;
 
 public class Scanner {
 
-    Scanner(String source){
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+
+    Scanner(String source) {
+        this.source = source;
+    }
+
+    List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            // 次の字句の先頭からはじめる
+            start = current;
+            scanToken();
+        }
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+
+    private boolean isAtEnd() {
+        return current > source.length();
+    }
+
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+            // 基本字句の解析を行う
+            case '(':
+                addToken(LEFT_PAREN);
+                break;
+            case ')':
+                addToken(RIGHT_PAREN);
+                break;
+            case '{':
+                addToken(LEFT_BRACE);
+                break;
+            case '}':
+                addToken(RIGHT_BRACE);
+                break;
+            case ',':
+                addToken(COMMA);
+                break;
+            case '.':
+                addToken(DOT);
+                break;
+            case '-':
+                addToken(MINUS);
+                break;
+            case '+':
+                addToken(PLUS);
+                break;
+            case ';':
+                addToken(SEMICOLON);
+                break;
+            // 演算子の解析を行う箇所
+            case '*':
+                addToken(STAR);
+                break;
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
+            // コメント or 除算演算子の解析を行う
+            case '/':
+                if (match('/')) {
+                    //コメントは行末まで続く想定
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                } else {
+                    addToken(SLASH);
+                }
+            case ' ':
+            case '\r':
+            case '\t':
+            case '\n':
+                line++;
+                break;
+            // 文字列リテラルの解析を行う
+            case '"':
+                string();
+                break;
+            default:
+                Lox.error(line, "Unexpected character.");
+                break;
+        }
+    }
+
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') {
+                line++;
+            }
+            advance();
+        }
+
+        if (isAtEnd()) {
+            Lox.error(line, "Unterminated string."); // 文字列が終結してない
+            return;
+        }
+
+        // 左右の引用符を切り捨てる
+        String value = source.substring(start + 1, current - 1);
+        addToken(STRING, value);
+    }
+
+    // 1文字の先読みを行う
+    private char peek() {
+        if (isAtEnd()) return '\0';
+        return source.charAt(current);
+    }
+
+    private boolean match(char expected) {
+        if (isAtEnd()) return false;
+        if (source.charAt(current) != expected) return false;
+        current++;
+        return true;
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
 
     }
-    List<Token> scanTokens(){
-        return Collections.emptyList();
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
     }
 
+    private char advance() {
+        return source.charAt(current++);
+    }
 }
