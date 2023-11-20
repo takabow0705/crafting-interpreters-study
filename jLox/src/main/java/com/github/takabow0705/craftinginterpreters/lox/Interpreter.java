f760c0fc529a2f02e5373b2312d4b172055b9c00
takabow0705 <hrgc.takahiro@outlook.jp>
2023-11-20 17:03:18 +0900

    connect to interpter

--- jLox/src/main/java/com/github/takabow0705/craftinginterpreters/lox/Interpreter.java
+++ jLox/src/main/java/com/github/takabow0705/craftinginterpreters/lox/Interpreter.java
@@ -1,6 +1,16 @@
 package com.github.takabow0705.craftinginterpreters.lox;
 
 public class Interpreter implements Expr.Visitor<Object> {
+
+    void interpret(Expr expression) {
+        try {
+            Object value = evaluate(expression);
+            System.out.println(stringify(value));
+        } catch (RuntimeError error) {
+            Lox.runtimeError(error);
+        }
+    }
+
     @Override
     public Object visitBinaryExpr(Expr.Binary expr) {
         Object left = evaluate(expr.left);
@@ -94,5 +104,18 @@ public class Interpreter implements Expr.Visitor<Object> {
         if (a == null && b == null) return true;
         if (a == null) return false;
         return a.equals(b);
+    }
+
+    private String stringify(Object object) {
+        if (object == null) return "nil";
+
+        if (object instanceof Double) {
+            String text = object.toString();
+            if (text.endsWith(".0")) {
+                text.substring(0, text.length() - 2);
+            }
+            return text;
+        }
+        return object.toString();
     }
 }
