c30e1b9624341e4c600370ab6e5a67285d716857
takabow0705 <hrgc.takahiro@outlook.jp>
2023-11-20 23:35:05 +0900

    add if statement

--- jLox/src/main/java/com/github/takabow0705/craftinginterpreters/lox/Interpreter.java
+++ jLox/src/main/java/com/github/takabow0705/craftinginterpreters/lox/Interpreter.java
@@ -121,6 +121,11 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     }
 
     @Override
+    public Void visitIfStmt(Stmt.If stmt) {
+        return null;
+    }
+
+    @Override
     public Void visitPrintStmt(Stmt.Print stmt) {
         Object value = evaluate(stmt.expression);
         System.out.println(stringify(value));
