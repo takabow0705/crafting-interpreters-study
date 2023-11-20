02cbbf4a5085a74dff9060d1a9915b798910e7d8
takabow0705 <hrgc.takahiro@outlook.jp>
2023-11-20 10:28:36 +0900

    create parser chpater6 p106

---
+++ jLox/src/main/java/com/github/takabow0705/craftinginterpreters/lox/Parser.java
@@ -0,0 +1,120 @@
+package com.github.takabow0705.craftinginterpreters.lox;
+
+import java.util.List;
+
+import static com.github.takabow0705.craftinginterpreters.lox.TokenType.*;
+
+public class Parser {
+    private final List<Token> tokens;
+    private int current = 0;
+
+    public Parser(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+
+    private Expr expression() {
+        return equality();
+    }
+
+    private Expr equality() {
+        Expr expr = comparison();
+        while (match(BANG_EQUAL, EQUAL_EQUAL)) {
+            Token operator = previous();
+            Expr right = comparison();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+        return expr;
+    }
+
+    private Token previous() {
+        return tokens.get(current - 1);
+    }
+
+    private Expr comparison() {
+        Expr expr = term();
+        while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+            Token operator = previous();
+            Expr right = term();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+        return expr;
+    }
+
+    private Expr term() {
+        Expr expr = factor();
+        while (match(MINUS, PLUS)) {
+            Token operator = previous();
+            Expr right = factor();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+        return expr;
+    }
+
+    private Expr factor() {
+        Expr expr = unary();
+        while (match(SLASH, STAR)) {
+            Token operator = previous();
+            Expr right = unary();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+        return expr;
+    }
+
+    private Expr unary() {
+        if (match(BANG, MINUS)) {
+            Token operator = previous();
+            Expr right = unary();
+            return new Expr.Unary(operator, right);
+        }
+        return primary();
+    }
+
+    private Expr primary() {
+        if (match(FALSE)) return new Expr.Literal(false);
+        if (match(TRUE)) return new Expr.Literal(true);
+        if (match(NIL)) return new Expr.Literal(null);
+        if (match(NUMBER, STRING)) {
+            return new Expr.Literal(previous().literal);
+        }
+
+        if (match(LEFT_PAREN)) {
+            Expr expr = expression();
+            consume(RIGHT_PAREN, "Expect ')' after expression.");
+            return new Expr.Grouping(expr);
+        }
+        throw new IllegalArgumentException(); //一時的においておく
+    }
+
+    private void consume(TokenType rightParen, String s) {
+    }
+
+
+    private boolean match(TokenType... tokenTypes) {
+        for (TokenType type : tokenTypes) {
+            if (check(type)) {
+                advance();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Token advance() {
+        if (!isAtEnd()) current++;
+        return previous();
+    }
+
+    private boolean check(TokenType type) {
+        if (isAtEnd()) return false;
+        return peek().tokenType == type;
+    }
+
+    private Token peek() {
+        return tokens.get(current);
+    }
+
+    private boolean isAtEnd() {
+        return peek().tokenType == EOF;
+    }
+
+}
