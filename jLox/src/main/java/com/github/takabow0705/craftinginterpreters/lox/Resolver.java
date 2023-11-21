a3472ffb80254a5eed1c298b0f1fe45e8af5ce70
takabow0705 <hrgc.takahiro@outlook.jp>
2023-11-22 01:28:03 +0900

    impl error handling when super class is not inferitted

--- jLox/src/main/java/com/github/takabow0705/craftinginterpreters/lox/Resolver.java
+++ jLox/src/main/java/com/github/takabow0705/craftinginterpreters/lox/Resolver.java
@@ -91,6 +91,11 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     @Override
     public Void visitSuperExpr(Expr.Super expr) {
+        if (currentClass == ClassType.NONE) {
+            Lox.error(expr.keyword, "Can't use 'super' outside of a class.");
+        } else if (currentClass != ClassType.SUBCLASS) {
+            Lox.error(expr.keyword, "Can't use 'super' in a class with no superclass.");
+        }
         resolveLocal(expr, expr.keyword);
         return null;
     }
@@ -125,6 +130,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         }
 
         if (stmt.superclass != null) {
+            currentClass = ClassType.SUBCLASS;
             resolve(stmt.superclass);
         }
 
@@ -280,6 +286,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     private enum ClassType {
         NONE,
-        CLASS
+        CLASS,
+        SUBCLASS
     }
 }
