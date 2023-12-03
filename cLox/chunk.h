b8039ea2feb09caf9e6b7be8fea3e341dc146a6b
takabow0705 <hrgc.takahiro@outlook.jp>
2023-12-03 14:58:41 +0000

    run daynamic array (p304)

--- cLox/chunk.h
+++ cLox/chunk.h
@@ -10,7 +10,13 @@ typedef enum
 
 typedef struct
 {
+    int count;
+    int capacity;
     uint8_t *code;
-} Chukk;
+} Chunk;
+
+void initChunk(Chunk *chunk);
+void freeChunk(Chunk *chunk);
+void writeChunk(Chunk *chunk, uint8_t byte);
 
 #endif
